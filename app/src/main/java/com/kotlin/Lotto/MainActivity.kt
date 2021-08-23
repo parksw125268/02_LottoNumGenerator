d87a87c15322be96ad641a0426a2fbc1af85f38e
CookDas2 <parksw125368@naver.com>
2021-08-24 07:38:34 +0900

    210824_2

--- app/src/main/java/com/kotlin/Lotto/MainActivity.kt
+++ app/src/main/java/com/kotlin/Lotto/MainActivity.kt
@@ -2,8 +2,11 @@ package com.kotlin.Lotto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.NumberPicker
+import android.widget.Toast
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
     private val clearButton : Button by  lazy {
@@ -18,6 +21,9 @@ class MainActivity : AppCompatActivity() {
     private val numberPicker : NumberPicker by lazy {
         findViewById<NumberPicker>(R.id.numberPicker)
     }
+    private var didRun = false //이미 실행되었는지 체크할 변수
+    private val pickNumberSet = hashSetOf<Int>()//똑같은 숫자는 더이상 Add되지 않도록
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -26,10 +32,25 @@ class MainActivity : AppCompatActivity() {
         numberPicker.maxValue = 45
 
         initRunButton()
+        initAddButton()
+    }
+    private fun initAddButton(){
+        addButton.setOnClickListener {
+            if(didRun){
+                Toast.makeText(this,"초기화 후에 시도해주세요 ", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (pickNumberSet.size >= 5){
+                Toast.makeText(this,"번호는 5개까지만 선택 할 수 있습니다. ", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+           // if (pickNumberSet.contains(numberPicker.value))
+        }
     }
     private fun initRunButton() {
         runButton.setOnClickListener(){
             val list = getRandomNumber()
+            Log.d("MainActivity",list.toString())
         }
     }
     private fun getRandomNumber(): List<Int> {
@@ -38,8 +59,8 @@ class MainActivity : AppCompatActivity() {
                 this.add(i)
             }
             this.shuffle()
-            this.subList(0,6)
-            return this
         }
+        val newList  = numberList.subList(0,6)
+        return newList.sorted()
     }
 }
